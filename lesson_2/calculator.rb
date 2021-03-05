dec6a3842c2a463ca3d82298675f4de6f6269922
Lisa Melo <melo.nutrition@gmail.com>
2021-03-05 12:45:41 -0500

    added greeting and validation

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,21 +1,106 @@
-puts "Please enter a number"
-num1 = gets.chomp.to_i
-
-puts "Please enter another number"
-num2 = gets.chomp.to_i
-
-puts "What oepration do you want to perform? add, subtract, multiply or divide?"
-operation = gets.chomp
-
-case operation
-when "add"
-  result = num1 + num2
-when "subtract"
-  result = num1 - num2
-when "multiply"
-  result = num1 * num2
-when "divide"
-  result = num1.to_f / num2.to_f
+def prompt(message)
+  puts "=> #{message}"
 end
 
-puts result
+def valid_number?(num)
+  num.to_i != 0
+end
+
+def operation_to_message(op)
+  case op
+  when "1"
+    "Adding"
+  when "2"
+    "Subtracting"
+  when "3"
+    "Multiplying"
+  when "4"
+    "Dividing"
+  end
+end
+
+prompt("Welcome to Calculator! Please enter your name")
+
+name = ''
+
+loop do
+  name = gets.chomp
+  if name.empty?
+    prompt("Make sure to use a valid name")
+  else
+    break
+  end
+end
+
+prompt("Hi, #{name.capitalize!}!")
+
+loop do
+  num1 = ''
+
+  loop do
+    prompt("What is the first number?")
+    num1 = gets.chomp
+    if valid_number?(num1)
+      num1 = num1.to_i
+      break
+    else
+      prompt("Hm.. that doesn't look like a valid number")
+    end
+  end
+
+  num2 = ''
+
+  loop do
+    prompt("What is the second number?")
+    num2 = gets.chomp
+
+    if valid_number?(num2)
+      num2 = num2.to_i
+      break
+    else
+      prompt("Hmm.. that doesn't look like a valid number")
+    end
+  end
+
+  operator_prompt = <<-MSG
+    What operation would you like to perform? 
+    1) add 
+    2) subtract 
+    3) multiply 
+    4) divide
+  MSG
+
+  prompt(operator_prompt)
+
+  operator = ''
+
+  loop do
+    operator = gets.chomp
+    if ['1', '2', '3', '4'].include?(operator)
+      break
+    else
+      prompt("Must choose 1, 2, 3, or 4.")
+    end
+  end
+
+  prompt("#{operation_to_message(operator)} the two numbers...")
+
+  result = case operator
+           when "1"
+             num1 + num2
+           when "2"
+             num1 - num2
+           when "3"
+             num1 * num2
+           when "4"
+             num1.to_f / num2.to_f
+           end
+
+  prompt("The result is #{result}")
+
+  prompt("Do you want to peform another calculation? (Y to calculate again)")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for using the calculator.")
