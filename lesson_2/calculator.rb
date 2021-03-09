fb205b16b99485b60f1a25ac6007b3abfbfc52cb
Lisa Melo <melo.nutrition@gmail.com>
2021-03-09 10:32:42 -0500

    fixed bug - case operator

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,16 +1,20 @@
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+
 def prompt(message)
   puts "=> #{message}"
 end
 
 def valid_number?(num)
   unless num == '0' 
-    num.to_i != 0
+    num.to_f != 0.0
   else true
   end
 end
 
 def valid_denominator?(num)
-  num.to_i != 0
+  num.to_f != 0.0
 end
 
 def operation_to_message(op)
@@ -26,14 +30,14 @@ def operation_to_message(op)
   end
 end
 
-prompt("Welcome to Calculator! Please enter your name")
+prompt(MESSAGES['welcome'])
 
 name = ''
 
 loop do
   name = gets.chomp
   if name.empty?
-    prompt("Make sure to use a valid name")
+    prompt(MESSAGES['valid_name'])
   else
     break
   end
@@ -45,27 +49,27 @@ loop do
   num1 = ''
 
   loop do
-    prompt("What is the first number?")
+    prompt(MESSAGES['first_number'])
     num1 = gets.chomp
     if valid_number?(num1)
-      num1 = num1.to_i
+      num1 = num1.to_f
       break
     else
-      prompt("Hm.. that doesn't look like a valid number")
+      prompt(MESSAGES['invalid_number'])
     end
   end
 
   num2 = ''
 
   loop do
-    prompt("What is the second number?")
+    prompt(MESSAGES['second_number'])
     num2 = gets.chomp
 
     if valid_number?(num2)
-      num2 = num2.to_i
+      num2 = num2.to_f
       break
     else
-      prompt("Hmm.. that doesn't look like a valid number")
+      prompt(MESSAGES['invalid_number'])
     end
   end
 
@@ -86,37 +90,36 @@ loop do
     if ['1', '2', '3', '4'].include?(operator)
       break
     else
-      prompt("Must choose 1, 2, 3, or 4.")
+      prompt(MESSAGES['operation_choice'])
     end
   end
 
   prompt("#{operation_to_message(operator)} the two numbers...")
 
-  result = ""
-  case operator
-  when "1"
-    result = num1 + num2
-  when "2"
-    result = num1 - num2
-  when "3"
-    result = num1 * num2
-  when "4"
-    loop do
-      if num2.to_i == 0
-        puts "This is not a valid denominator. Please enter a non-zero number"
-        num2 = gets.chomp
-      else 
-        result = num1.to_f / num2.to_f
-        break
-      end
-    end
-  end
+  result =  case operator
+            when "1"
+              num1 + num2
+            when "2"
+              num1 - num2
+            when "3"
+              num1 * num2
+            when "4"
+              loop do
+                if num2.to_f == 0.0
+                puts MESSAGES['valid_denominator']
+                num2 = gets.chomp.to_f
+                else 
+                  break
+                end
+              end
+            num1 / num2
+            end
 
   prompt("The result is #{result}")
 
-  prompt("Do you want to peform another calculation? (Y to calculate again)")
+  prompt(MESSAGES['another_calculation'])
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt("Thank you for using the calculator.")
+prompt(MESSAGES['thank_you'])
